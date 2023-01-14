ad9c451acf71214d5cae6dadc6fdb19a699f8d3a
Abdullah <Abdullah Al-araj@hotmail.com>
2023-01-14 15:33:10 +0200

    Added add patients validation

---
+++ DentalClinicProject/Patient.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalClinicProject
+{
+    internal class Patient
+    {
+        private int id;
+        private string name;
+        private string email;
+        private string dateOfBirth;
+        private string gender;
+        private string nextAppointment;
+
+        public Patient(string name, string email, string dateOfBirth, string gender)
+        {
+            this.name = name;
+            this.email = email;
+            this.dateOfBirth = dateOfBirth;
+            this.gender = gender;
+            id = -1;
+            nextAppointment = null;
+
+        }
+        public int GetID()
+        {
+            return id;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetEmail()
+        {
+            return email;
+        }
+
+        public string GetDateOfBirth()
+        {
+            return dateOfBirth;
+        }
+
+        public string GetGender()
+        {
+            return gender;
+        }
+
+        public string GetNextAppointment()
+        {
+            return nextAppointment;
+        }
+
+        public void SetNextAppointment(string appointment)
+        {
+            nextAppointment = appointment;
+        }
+
+        public void SetID(int id)
+        {
+            this.id = id;
+        }
+
+    }
+}
